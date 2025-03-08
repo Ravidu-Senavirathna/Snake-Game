10083aff3c195c9da6c113bce2f03b69aaf49a12
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-03-08 10:00:00 +0530

    initialz food class

--- game/Food.py
+++ game/Food.py
@@ -7,7 +7,9 @@ from Constants import *
 class Food(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        pass
+        self.x = random.randint(0, SCREEN_WIDTH - FOOD_SIZE[0])
+        self.y = random.randint(0, SCREEN_HEIGHT - FOOD_SIZE[1])
+        self.food_rect = pygame.Rect(self.x, self.y, *FOOD_SIZE) # Create a rect for the food to handle collisions
 
     def draw(self, screen):
         pass
