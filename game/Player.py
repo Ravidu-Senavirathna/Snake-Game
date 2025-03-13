654eb24a57f5ac36924e9b68228c20524c6981a2
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-03-13 10:00:00 +0530

    updated documentation for player.py

--- game/Player.py
+++ game/Player.py
@@ -5,12 +5,7 @@ import os
 import Constants
 
 
-''' 
-Initialize the player's starting position to the center of the screen. 
-The player will be represented as a rectangle with a specific size defined in Constants.py. 
-The starting position is calculated to ensure that the player is centered on the screen when the game starts.
-'''
-
+# Import necessary modules and constants for the Player class
 STARTING_POSITION_X = (Constants.SCREEN_WIDTH // 2) - (Constants.PLAYER_SIZE[0] // 2)
 STARTING_POSITION_Y = (Constants.SCREEN_HEIGHT // 2) - (Constants.PLAYER_SIZE[1] // 2)
 
@@ -20,19 +15,18 @@ PLAYER_SIZE = Constants.PLAYER_SIZE
 
 
 
-
 '''
-The Player class represents the player character in the game. 
-It is responsible for handling the player's position, movement, and drawing the player on the screen. 
-The player is represented as a simple rectangle for now, but this can be replaced with an image or sprite later on. 
+The Player class inherits from pygame.sprite.Sprite, 
+which allows it to be used in conjunction with Pygame's sprite groups and collision detection features.
+
+Main methods of the Player class include:
+    - __init__: Initializes the player with a starting position and creates a rectangle for collision detection 
+    - draw: Draws the player on the screen as a rectangle filled with the specified color
+    - move: Moves the player by a certain amount in the x and y directions and updates the player's rect position accordingly   
 
 The class also includes methods: 
     - getting the player's rect for collision detection and boundary checking 
     - getting and setting the player's position.  
-
-    
-    The Player class inherits from pygame.sprite.Sprite, 
-    which allows it to be used in conjunction with Pygame's sprite groups and collision detection features.
 
 '''
 
