42af3fe3d386f8f9e9571801fbd7c5edd382452a
Ravidu Senavirathna <ravidupasan11@gmail.com>
2025-03-20 10:00:00 +0530

    added draw grid for util

---
+++ game/Util.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import random
+import pygame
+
+import Constants
+
+
+
+'''Draws a grid on the given surface using the specified box size from Constants.
+This function is used to visually divide the game screen into a grid, which can help with movement and collision detection.'''
+
+def draw_grid(surface, 
+              box_size=Constants.BOX_SIZE, 
+              color=Constants.GRAY, 
+              width= Constants.SCREEN_WIDTH, 
+              height=Constants.SCREEN_HEIGHT
+              ):
+    
+    for x in range(0, width, box_size):
+        pygame.draw.line(surface, color, (x, 0), (x, height))
+    for y in range(0, height, box_size):
+        pygame.draw.line(surface, color, (0, y), (width, y))
+
